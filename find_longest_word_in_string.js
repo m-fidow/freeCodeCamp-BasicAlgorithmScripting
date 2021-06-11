917ff81197ee645db6a93dce2a4cd1a1d815b6f4
m-fidow <muna.a.fidow@gmail.com>
2021-06-11 16:31:37 +0100

    plan

--- find_longest_word_in_string.js
+++ find_longest_word_in_string.js
@@ -1,7 +1,10 @@
 // Return the length of the longest word in the provided sentence.
 
 // Your response should be a number.
-
+//split string into array
+//loop though array
+//sort items in decending order by length
+//return first item of sorted array
 function findLongestWordLength(str) {
   return str.length;
 }
