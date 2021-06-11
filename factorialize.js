01383672129433fa9b0ffbf56de2e85a3ca4aac6
m-fidow <muna.a.fidow@gmail.com>
2021-06-11 16:18:59 +0100

    refactored into fat arrow

--- factorialize.js
+++ factorialize.js
@@ -1,10 +1,19 @@
 //start at 1
 //multiply number to the following number
 //multiply result to following number until you reach num
-function factorialize(num) {
+// function factorialize(num) {
+//   let result = 1;
+//   for (let i = 0; i < num; i++) {
+//     result *= i + 1;
+//   }
+//   return result;
+// }
+
+let factorialize = (num) => {
   let result = 1;
   for (let i = 0; i < num; i++) {
     result *= i + 1;
   }
   return result;
-}
+};
+factorialize(5);
