2d9cdcdb6f6374ce938ea6ba61473e612994fb8a
m-fidow <muna.a.fidow@gmail.com>
2021-06-11 16:04:58 +0100

    plan

---
+++ factorialize.js
@@ -0,0 +1,8 @@
+//start at 1
+//multiply number to the following number
+//multiply result to following number until you reach num
+function factorialize(num) {
+  return num;
+}
+
+factorialize(5);
