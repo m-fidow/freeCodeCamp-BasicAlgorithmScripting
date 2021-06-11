6dddf1e05477b4da81930416017db187c8b57f65
m-fidow <muna.a.fidow@gmail.com>
2021-06-11 16:15:04 +0100

    passed

--- factorialize.js
+++ factorialize.js
@@ -2,7 +2,9 @@
 //multiply number to the following number
 //multiply result to following number until you reach num
 function factorialize(num) {
-  return num;
+  let result = 1;
+  for (let i = 0; i < num; i++) {
+    result *= i + 1;
+  }
+  return result;
 }
-
-factorialize(5);
