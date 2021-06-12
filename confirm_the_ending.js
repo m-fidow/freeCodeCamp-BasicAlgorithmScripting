bbd8f88134e84d3f5fcdba8d9333b7605643fa43
m-fidow <muna.a.fidow@gmail.com>
2021-06-12 19:02:00 +0100

    plan

---
+++ confirm_the_ending.js
@@ -0,0 +1,12 @@
+//check target length
+//use substring to return the ending of string
+//  substring(startIndex) will be target.length -1
+//  set as ending_of_string
+//if target and ending_of_string are the same return true
+//else, return false
+
+function confirmEnding(str, target) {
+  return str;
+}
+
+confirmEnding("Bastian", "n");
