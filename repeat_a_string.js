381024423d168f604c8cd63441b34d60897a15e4
m-fidow <muna.a.fidow@gmail.com>
2021-06-12 19:26:24 +0100

    plan

---
+++ repeat_a_string.js
@@ -0,0 +1,12 @@
+//create an empty erray
+//if num is < 0 return ""
+//create for loop
+//push to array as many times as num
+//join array
+//return joined array
+
+function repeatStringNumTimes(str, num) {
+  return str;
+}
+
+repeatStringNumTimes("abc", 3);
