f41e22923fcb1df0404b828e9882382517f7fee8
m-fidow <muna.a.fidow@gmail.com>
2021-06-12 19:47:21 +0100

    plan

---
+++ truncate_string.js
@@ -0,0 +1,12 @@
+//split string into array
+//set as arrayOfLetters
+//if arrayOfLetters.length > num
+//splice arrayOfLetters (0,num)
+//set as splicedArray
+//return splicedArray
+
+function truncateString(str, num) {
+  return str;
+}
+
+truncateString("A-tisket a-tasket A green and yellow basket", 8);
