2d60c3e3139f6f27dde762b8a457051855a1bfff
m-fidow <muna.a.fidow@gmail.com>
2021-06-12 19:58:52 +0100

    refactored with ternary operator

--- truncate_string.js
+++ truncate_string.js
@@ -5,13 +5,19 @@
 //set as splicedArray
 //return join splicedArray and add truncation signs at the end
 
+// function truncateString(str, num) {
+//   let arrayOfLetters = str.split("");
+//   if (arrayOfLetters.length > num) {
+//     let spliced = arrayOfLetters.splice(0, num);
+//     return spliced.join("") + "...";
+//   } else {
+//     return str;
+//   }
+// }
 function truncateString(str, num) {
   let arrayOfLetters = str.split("");
-  if (arrayOfLetters.length > num) {
-    let spliced = arrayOfLetters.splice(0, num);
-    return spliced.join("") + "...";
-  } else {
-    return str;
-  }
+  return arrayOfLetters.length > num
+    ? arrayOfLetters.splice(0, num).join("") + "..."
+    : str;
 }
 truncateString("A-tisket a-tasket A green and yellow basket", 8);
