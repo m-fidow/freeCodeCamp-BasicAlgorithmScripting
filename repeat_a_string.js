034a447987dd204b0bd8d7a423433d0faaf36839
m-fidow <muna.a.fidow@gmail.com>
2021-06-12 19:27:09 +0100

    passed

--- repeat_a_string.js
+++ repeat_a_string.js
@@ -2,11 +2,19 @@
 //if num is < 0 return ""
 //create for loop
 //push to array as many times as num
+//
 //join array
 //return joined array
 
 function repeatStringNumTimes(str, num) {
-  return str;
+  if (num < 0) {
+    return "";
+  }
+  let array = [];
+  for (let i = 0; i < num; i++) {
+    array.push(str);
+  }
+  return array.join("");
 }
 
 repeatStringNumTimes("abc", 3);
