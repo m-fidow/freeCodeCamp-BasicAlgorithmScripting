f8099087b7d623f57f6ec004e0233306273e5139
m-fidow <muna.a.fidow@gmail.com>
2021-06-12 18:42:18 +0100

    passed

---
+++ return_largest_number_in_array.js
@@ -0,0 +1,23 @@
+//loop through the big array
+//loop thought the small array
+//sort numbers from lowest to highest
+//push highest number in finalarray
+//return final array
+
+function largestOfFour(arr) {
+  let finalArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    let largestNumberInEachArray = arr[i].sort((a, b) => {
+      return b - a;
+    });
+    finalArray.push(largestNumberInEachArray[0]);
+  }
+  return finalArray;
+}
+
+largestOfFour([
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1],
+]);
