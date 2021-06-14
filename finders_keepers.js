e97d9ce768e9bc552525eb3c0900f908ea1ee20f
m-fidow <muna.a.fidow@gmail.com>
2021-06-14 20:19:48 +0100

    plan

---
+++ finders_keepers.js
@@ -0,0 +1,11 @@
+//loop though array
+//check if func is true for each element of the array
+//if true, return array element
+//if false, return undefined
+
+function findElement(arr, func) {
+  let num = 0;
+  return num;
+}
+
+findElement([1, 2, 3, 4], (num) => num % 2 === 0);
