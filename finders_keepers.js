e4295e79ab696f082b9788a8ebad332155626435
m-fidow <muna.a.fidow@gmail.com>
2021-06-14 20:20:36 +0100

    passed

--- finders_keepers.js
+++ finders_keepers.js
@@ -5,7 +5,13 @@
 
 function findElement(arr, func) {
   let num = 0;
-  return num;
+  for (let i = 0; i < arr.length; i++) {
+    // num = arr[i]
+    if (func(arr[i])) {
+      num = arr[i];
+      return num;
+    }
+  }
 }
 
 findElement([1, 2, 3, 4], (num) => num % 2 === 0);
