569d76c7d1b37e4a15b781bdc2cd15f72394f2e3
m-fidow <muna.a.fidow@gmail.com>
2021-06-14 20:31:29 +0100

    passed

--- boo_who.js
+++ boo_who.js
@@ -1,7 +1,11 @@
 //if typeof bool is boolean return true
 //else return false
 function booWho(bool) {
-  return bool;
+  if (typeof bool == "boolean") {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 booWho(null);
