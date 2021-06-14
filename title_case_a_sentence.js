9572a37832e94e773f0d76b160a84e6b94b7e318
m-fidow <muna.a.fidow@gmail.com>
2021-06-14 20:52:48 +0100

    refactored to use forEach

--- title_case_a_sentence.js
+++ title_case_a_sentence.js
@@ -7,16 +7,26 @@
 //rejoin arrayOfWords into string
 //return string
 
+// function titleCase(str) {
+//   let arrayOfWords = str.toLowerCase().split(" ");
+//   // console.log(arrayOfWords)
+//   let arrayOfCapitalisedWords = [];
+//   for (let i = 0; i < arrayOfWords.length; i++) {
+//     arrayOfCapitalisedWords.push(
+//       arrayOfWords[i].charAt(0).toUpperCase() + arrayOfWords[i].substring(1)
+//     );
+//   }
+//   // console.log(arrayOfCapitalisedWords.join(" "))
+//   return arrayOfCapitalisedWords.join(" ");
+// }
 function titleCase(str) {
   let arrayOfWords = str.toLowerCase().split(" ");
-  // console.log(arrayOfWords)
+
   let arrayOfCapitalisedWords = [];
-  for (let i = 0; i < arrayOfWords.length; i++) {
-    arrayOfCapitalisedWords.push(
-      arrayOfWords[i].charAt(0).toUpperCase() + arrayOfWords[i].substring(1)
-    );
-  }
-  // console.log(arrayOfCapitalisedWords.join(" "))
+  arrayOfWords.forEach((x) =>
+    arrayOfCapitalisedWords.push(x.charAt(0).toUpperCase() + x.substring(1))
+  );
+
   return arrayOfCapitalisedWords.join(" ");
 }
 
