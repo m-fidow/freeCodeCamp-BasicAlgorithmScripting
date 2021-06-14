595d96c94a4442907f08e566a1f063c5c0236ea6
m-fidow <muna.a.fidow@gmail.com>
2021-06-14 19:40:24 +0100

    refactored to slimmer for loop

--- return_largest_numbers_in_arrays.js
+++ return_largest_numbers_in_arrays.js
@@ -5,13 +5,20 @@
 //sort numbers from highest to lowest
 //push first item of array into largestFourArray
 //return largestFourArray
+// function largestOfFour(arr) {
+//   let largestFourArray = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     let smallArray = arr[i];
+//     // console.log(smallArray)
+//     let sortedArray = smallArray.sort((a, b) => b - a);
+//     largestFourArray.push(sortedArray[0]);
+//   }
+//   return largestFourArray;
+// }
 function largestOfFour(arr) {
   let largestFourArray = [];
   for (let i = 0; i < arr.length; i++) {
-    let smallArray = arr[i];
-    // console.log(smallArray)
-    let sortedArray = smallArray.sort((a, b) => b - a);
-    largestFourArray.push(sortedArray[0]);
+    largestFourArray.push(arr[i].sort((a, b) => b - a)[0]);
   }
   return largestFourArray;
 }
