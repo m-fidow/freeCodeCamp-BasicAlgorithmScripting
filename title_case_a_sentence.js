a914e8346ff990605b4040f799a2e2669facec62
m-fidow <muna.a.fidow@gmail.com>
2021-06-14 20:36:23 +0100

    plan

---
+++ title_case_a_sentence.js
@@ -0,0 +1,14 @@
+//split string into array of words
+//set as ArrayOfWords
+//lowercase each element
+//isolate first letter of each item in ArrayOfWords
+//capitalise first letter
+//rejoin first letter with rest of words
+//rejoin arrayOfWords into string
+//return string
+
+function titleCase(str) {
+  return str;
+}
+
+titleCase("I'm a little tea pot");
