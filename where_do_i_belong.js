608ebf6b11cc2d33c2630c4cb6b90998fd4e2bc2
m-fidow <muna.a.fidow@gmail.com>
2021-06-15 21:31:28 +0100

    passed

--- where_do_i_belong.js
+++ where_do_i_belong.js
@@ -2,7 +2,9 @@
 //sort arr from lowest to highest
 //return index of num
 function getIndexToIns(arr, num) {
-  return num;
+  arr.push(num);
+  let sorted = arr.sort((a, b) => a - b);
+  return sorted.indexOf(num);
 }
 
 getIndexToIns([40, 60], 50);
