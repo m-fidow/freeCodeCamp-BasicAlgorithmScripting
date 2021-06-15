d32028106fc8d3b9d2dafd677b23657dc6aeda5e
m-fidow <muna.a.fidow@gmail.com>
2021-06-15 21:11:40 +0100

    plan

---
+++ slice_and_splice.js
@@ -0,0 +1,10 @@
+//copy arr1 into arr2 from n index
+//create a copy of arr2 so that the original remains the same
+//loop through arr1 and insert each item into copy of array 2 starting from n using splice
+//return copy of arr2 that now is transformed because of splice
+
+function frankenSplice(arr1, arr2, n) {
+  return arr2;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
