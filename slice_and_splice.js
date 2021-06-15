47f67cef25416a17229ed5ea259e85ef07a65749
m-fidow <muna.a.fidow@gmail.com>
2021-06-15 21:12:15 +0100

    passed

--- slice_and_splice.js
+++ slice_and_splice.js
@@ -4,7 +4,15 @@
 //return copy of arr2 that now is transformed because of splice
 
 function frankenSplice(arr1, arr2, n) {
-  return arr2;
+  // let newArray = [secondArray[0]]
+
+  let copyOfSecondArray = arr2.slice(0);
+  for (let i = 0; i < arr1.length; i++) {
+    copyOfSecondArray.splice(n, 0, arr1[i]);
+    n++;
+  }
+  return copyOfSecondArray;
+  // console.log({slicedArray})
 }
 
 frankenSplice([1, 2, 3], [4, 5, 6], 1);
